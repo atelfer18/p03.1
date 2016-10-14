04be842c5cae27eb697ff7b85d3c1ee77d2d349e
abigail telfer <atelfer18@VenerableBede.schools.sunderland.gov.uk>
2016-10-14 09:57:49 +0100

    work for p03.1

--- multiple_count.py
+++ multiple_count.py
@@ -27,3 +27,32 @@ def run_tests():
 
 # Edit this code
 def mult_count(n):
+
+    count =  0
+    
+    count5 = 0
+    count7 = 0
+    count11 = 0
+
+    for i in range(1, n+1, 1):
+        ## need to add in for remainders for 5, 7 and 11 so do not get double count
+
+        if i % 5 == 0 or i % 7 == 0 or i % 11 == 0
+
+    for i in range(5, n+1, 5):
+
+        count5 = count5 + 1
+        
+    for i in range(7, n+1, 7):
+
+        count7 = count7 + 1
+        
+    for i in range(11, n+1, 11):
+                    
+            count11 = count11 + 1
+
+    count = count5 + count7 + count11
+
+    print(count)
+
+    
